5e0ae99e81c13ac852d9e264f38baafd0c70b4c0
Tulio <tulio.mminini@gmail.com>
2023-02-16 20:46:22 -0300

    :lipstick: navbar responsive for smartphones

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-scroll";
 import style from "./navbar.module.css";
 
-
+import { IoMenu } from "react-icons/io5";
 
 export function Navbar() {
+  // menu hamburger
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle(style.responsive);
+  };
 
   // change nav background color when scrolling
   const [navColor, setNavColor] = useState(false);
@@ -16,10 +21,14 @@ export function Navbar() {
     }
   };
 
-  window.addEventListener("scroll", changeColor)
+  window.addEventListener("scroll", changeColor);
 
   return (
-    <nav className={ `${navColor ? style.navbar_background : style.navbar_container}`}>
+    <nav
+      className={`${
+        navColor ? style.navbar_background : style.navbar_container
+      }`}
+    >
       <Link
         to="hello"
         style={{ textDecoration: "none", background: "transparent" }}
@@ -30,44 +39,51 @@ export function Navbar() {
           <span>&#62;</span>tm
         </h2>
       </Link>
-
-      <div className={style.navbar__ul}>
-        <ul>
-          <Link
-            to="about"
-            offset={-70}
-            style={{ textDecoration: "none", background: "transparent" }}
-            smooth={true}
-          >
-            <li>About</li>
-          </Link>
-          <Link
-            to="skills"
-            offset={-70}
-            style={{ textDecoration: "none", background: "transparent" }}
-            smooth={true}
-          >
-            <li>Skills</li>
-          </Link>
-          <Link
-            to="portfolio"
-            offset={-70}
-            style={{ textDecoration: "none", background: "transparent" }}
-            smooth={true}
-          >
-            <li>Portfolio</li>
-          </Link>
-          <Link
-            to="contact"
-            style={{
-              textDecoration: "none",
-            }}
-            smooth={true}
-          >
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </div>
+      <ul ref={navRef} className={`${style.navbar__ul} ${style.nav_hidden}`}>
+        <Link
+          to="about"
+          offset={-70}
+          style={{ textDecoration: "none", background: "transparent" }}
+          smooth={true}
+          onClick={showNavbar}
+        >
+          <li>About</li>
+        </Link>
+        <Link
+          to="skills"
+          offset={-70}
+          style={{ textDecoration: "none", background: "transparent" }}
+          smooth={true}
+          onClick={showNavbar}
+        >
+          <li>Skills</li>
+        </Link>
+        <Link
+          to="portfolio"
+          offset={-70}
+          style={{ textDecoration: "none", background: "transparent" }}
+          smooth={true}
+          onClick={showNavbar}
+        >
+          <li>Portfolio</li>
+        </Link>
+        <Link
+          to="contact"
+          style={{
+            textDecoration: "none",
+          }}
+          smooth={true}
+          onClick={showNavbar}
+        >
+          <li>Contact</li>
+        </Link>
+      </ul>
+      <button
+        className={`${style.nav_btn} ${style.nav_close_btn}`}
+        onClick={showNavbar}
+      >
+        <IoMenu size={45} />
+      </button>
     </nav>
   );
 }
