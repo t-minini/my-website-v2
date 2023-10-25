0b8c2512e8aa7c7d9d4e28c1d27305220e308415
Tulio <tulio.mminini@gmail.com>
2023-10-25 22:42:45 +0100

    :lipstick: interests updated

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,25 +1,28 @@
-import style from "./skills.module.css";
-import css from "./../../assets/icons/css.svg";
-import html from "./../../assets/icons/html.svg";
-import sass from "./../../assets/icons/sass.svg";
-import jest from "./../../assets/icons/jest.png";
-import react from "./../../assets/icons/react.svg";
-import figma from "./../../assets/icons/figma.svg";
-import docker from "./../../assets/icons/docker.svg";
-import nodejs from "./../../assets/icons/nodejs.svg";
-import mui from "./../../assets/icons/material-ui.svg";
-import github from "./../../assets/icons/github-1.svg";
-import angular from "./../../assets/icons/angular.svg";
-import mongodb from "./../../assets/icons/mongodb.svg";
-import firebase from "./../../assets/icons/firebase.svg";
-import portuguese from "./../../assets/icons/brazil.svg";
-import bootstrap from "./../../assets/icons/bootstrap.svg";
-import javascript from "./../../assets/icons/javascript.svg";
-import typescript from "./../../assets/icons/typescript.svg";
-import swift from "./../../assets/icons/swift.svg"
-import reactNative from "./../../assets/icons/react-native.svg";
-import redux from "./../../assets/icons/redux.svg"
-import vue from "./../../assets/icons/vue.svg"
+import style from './skills.module.css';
+import css from './../../assets/icons/css.svg';
+import vue from './../../assets/icons/vue.svg';
+import aws from './../../assets/icons/aws.svg';
+import git from './../../assets/icons/git.svg';
+import html from './../../assets/icons/html.svg';
+import sass from './../../assets/icons/sass.svg';
+import jest from './../../assets/icons/jest.png';
+import react from './../../assets/icons/react.svg';
+import figma from './../../assets/icons/figma.svg';
+import swift from './../../assets/icons/swift.svg';
+import redux from './../../assets/icons/redux.svg';
+import docker from './../../assets/icons/docker.svg';
+import nodejs from './../../assets/icons/nodejs.svg';
+import python from './../../assets/icons/python.svg';
+import mui from './../../assets/icons/material-ui.svg';
+import github from './../../assets/icons/github-1.svg';
+import angular from './../../assets/icons/angular.svg';
+import mongodb from './../../assets/icons/mongodb.svg';
+import firebase from './../../assets/icons/firebase.svg';
+import portuguese from './../../assets/icons/brazil.svg';
+import bootstrap from './../../assets/icons/bootstrap.svg';
+import javascript from './../../assets/icons/javascript.svg';
+import typescript from './../../assets/icons/typescript.svg';
+import reactNative from './../../assets/icons/react-native.svg';
 
 export function Skills() {
   return (
@@ -65,11 +68,23 @@ export function Skills() {
               <img src={github} alt="github icon in black color" />
               <p>github</p>
             </div>
+            <div>
+              <img src={git} alt="git icon in orange color" />
+              <p>git</p>
+            </div>
           </div>
         </div>
         <div className={style.skills__learning}>
           <h3>interests:</h3>
           <div className={style.skills__icons}>
+            <div>
+              <img src={python} alt="python icon in yellow and blue colors" />
+              <p>python</p>
+            </div>
+            <div>
+              <img src={aws} alt="aws icon in black and orange colors" />
+              <p>aws</p>
+            </div>
             <div>
               <img src={sass} alt="sass icon in pink color" />
               <p>sass</p>
